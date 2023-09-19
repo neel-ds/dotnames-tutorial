19ee6c8299ffcb12cf573fd1b25aa94b54defc7b
Neel <neelp0980@gmail.com>
2023-09-19 20:54:05 +0530

    feat: sow-sdk implementation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
+import { SoWsdk } from "sow-sdk";
+import { SupportedChains } from "sow-sdk/dist/types";
 
 export default function Home() {
   const [walletAddress, setWalletAddress] = useState("");
   const [domainName, setDomainName] = useState("");
+  const [result, setResult] = useState("");
   return (
     <main className="flex flex-col w-[100%] p-10 justify-center items-center space-y-4">
       <h1 className="text-2xl">DotNames Tutorial</h1>
@@ -18,14 +21,27 @@ export default function Home() {
         />
         <button
           className="px-3 py-2 w-fit bg-[#1aa7ec] rounded-lg"
-          onChange={() => {}}
+          onClick={() => {
+            const sow = new SoWsdk();
+            console.log(walletAddress);
+            sow
+              .resolveName(walletAddress, SupportedChains.DotBit)
+              .then((res) => {
+                console.log(res);
+                setResult("Name Service: " + res);
+              })
+              .catch((err) => {
+                console.log(err);
+                setResult("Error");
+              });
+          }}
         >
           Find NS
         </button>
       </div>
       <div className="flex flex-col w-[40%] p-5 justify-center items-center border border-gray-400 rounded-xl space-y-4">
         <input
-          id="domain"
+          id="domainName"
           type="text"
           className="w-[90%] text-gray-700 border border-gray-400 p-2 rounded"
           placeholder="Enter your domain"
@@ -35,12 +51,25 @@ export default function Home() {
         />
         <button
           className="px-3 py-2 w-fit bg-[#1aa7ec] rounded-lg"
-          onChange={() => {}}
+          onClick={() => {
+            const sow = new SoWsdk();
+            sow
+              .resolveAddress(domainName)
+              .then((res) => {
+                console.log(res);
+                setResult("Address: " + res);
+              })
+              .catch((err) => {
+                console.log(err);
+                setResult("Error");
+              });
+          }}
         >
           Resolve
         </button>
       </div>
       <p className="text-xl">Results:</p>
+      <p className="text-lg">{result}</p>
     </main>
   );
 }
