9c46d95bccac255c8cc39885065a5d9d1771f8dc
Neel <neelp0980@gmail.com>
2023-09-20 21:50:20 +0530

    refactor: updated UX

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { useState } from "react";
 import { SoWsdk } from "sow-sdk";
 import { SupportedChains } from "sow-sdk/dist/types";
@@ -6,10 +7,22 @@ export default function Home() {
   const [walletAddress, setWalletAddress] = useState("");
   const [domainName, setDomainName] = useState("");
   const [result, setResult] = useState("");
+  const [selectedChain, setSelectedChain] = useState();
+  const [chain, setChain] = useState(1);
+
+  // Event handler to update the selected chain
+  const handleChainChange = (event: any) => {
+    setSelectedChain(event.target.value);
+    setChain(parseInt(event.target.value));
+  };
+
   return (
     <main className="flex flex-col w-[100%] p-10 justify-center items-center space-y-4">
-      <h1 className="text-2xl">DotNames Tutorial</h1>
-      <div className="flex flex-col w-[40%] p-5 justify-center items-center border border-gray-400 rounded-xl space-y-4">
+      <div className="flex flex-row mb-5 items-end space-x-4">
+        <Image src="/dotnames.png" width={70} height={70} alt="logo" />
+        <h1 className="text-2xl font-bold">DotNames Tutorial</h1>
+      </div>
+      <div className="flex flex-col w-[90%] md:w-[40%] sm:w-[50%] p-4 md:p-5 justify-center items-center border border-gray-400 rounded-xl space-y-4">
         <input
           id="address"
           type="text"
@@ -19,13 +32,33 @@ export default function Home() {
             setWalletAddress(e.target.value);
           }}
         />
+        <select
+          className="w-[90%] p-2 text-[#1e1e1e] rounded"
+          value={selectedChain}
+          onChange={handleChainChange}
+        >
+          <option value={SupportedChains.ENS}>ENS</option>
+          <option value={SupportedChains.SpaceId}>SpaceId</option>
+          <option value={SupportedChains.UnstoppableDomains}>
+            UnstoppableDomains
+          </option>
+          <option value={SupportedChains.DotBit}>DotBit</option>
+          <option value={SupportedChains.Zkns}>Zkns</option>
+          <option value={SupportedChains.ICNS}>ICNS</option>
+          <option value={SupportedChains.StargazeDomains}>
+            StargazeDomains
+          </option>
+          <option value={SupportedChains.Bonfida}>Bonfida</option>
+          <option value={SupportedChains.SuiNs}>SuiNs</option>
+          <option value={SupportedChains.AptosNs}>AptosNs</option>
+          <option value={SupportedChains.None}>None</option>
+        </select>
         <button
-          className="px-3 py-2 w-fit bg-[#1aa7ec] rounded-lg"
+          className="px-3 py-2 w-fit bg-[#f95b8b] rounded-lg"
           onClick={() => {
             const sow = new SoWsdk();
-            console.log(walletAddress);
             sow
-              .resolveName(walletAddress, SupportedChains.DotBit)
+              .resolveName(walletAddress, chain)
               .then((res) => {
                 console.log(res);
                 setResult("Name Service: " + res);
@@ -39,7 +72,7 @@ export default function Home() {
           Find NS
         </button>
       </div>
-      <div className="flex flex-col w-[40%] p-5 justify-center items-center border border-gray-400 rounded-xl space-y-4">
+      <div className="flex flex-col w-[90%] md:w-[40%] sm:w-[50%] p-4 md:p-5 justify-center items-center border border-gray-400 rounded-xl space-y-4">
         <input
           id="domainName"
           type="text"
@@ -50,7 +83,7 @@ export default function Home() {
           }}
         />
         <button
-          className="px-3 py-2 w-fit bg-[#1aa7ec] rounded-lg"
+          className="px-3 py-2 w-fit bg-[#f95b8b] rounded-lg"
           onClick={() => {
             const sow = new SoWsdk();
             sow
@@ -68,7 +101,19 @@ export default function Home() {
           Resolve
         </button>
       </div>
-      <p className="text-xl">Results:</p>
+      <div className="flex flex-row space-x-4 items-center">
+        <p className="text-xl font-semibold">Results:</p>
+        <Image
+          className="hover:cursor-pointer"
+          onClick={() => {
+            navigator.clipboard.writeText(result.split(" ").pop() || "");
+          }}
+          src="copy.svg"
+          width={15}
+          height={15}
+          alt="copy"
+        />
+      </div>
       <p className="text-lg">{result}</p>
     </main>
   );
